7d116ac04d2fce0f3ff7fe07c197e2bdd5ae1c34
souvikmallik <souvikmallik@localhost>
2023-06-30 12:30:57 +0530

    First comment

---
+++ src/Common_Api_Method/Patch_API_Methods.java
@@ -0,0 +1,24 @@
+package Common_Api_Method;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+
+public class Patch_API_Methods {
+public static int ResponsestatusCode(String BaseURI,String Resourse,String RequestBody) {
+		
+		RestAssured.baseURI=BaseURI;
+				
+	 int statusCode  = given().header("Content-Type", "application/json").body(RequestBody).                
+	              when().patch(Resourse).then().extract(). statusCode();                             
+	                  return statusCode ;
+	}
+	     public static String ResponseBody(String BaseURI,String Resourse,String RequestBody) {
+	              		
+	         	RestAssured.baseURI=BaseURI;
+	              				
+	      String ResponseBody  = given().header("Content-Type", "application/json").body(RequestBody).                
+	               when().patch(Resourse).then().extract().response().asPrettyString();                             
+	              	   return ResponseBody ;
+	}
+}
---
+++ src/Test_Classes/Get_TC1.java
@@ -0,0 +1,17 @@
+package Test_Classes;
+
+
+import Common_Api_Method.Get_API_Method;
+import Request_Repository.Get_Req_Repository;
+
+public class Get_TC1 {
+	public static void extractor() {
+
+		int Statuscode=Get_API_Method.ResponseStatusCode(Get_Req_Repository.baseURI(), Get_Req_Repository.Get_Resource());
+		System.out.println(Statuscode);
+		
+		
+		String ResponseBody=Get_API_Method.ResponseBody(Get_Req_Repository.baseURI(), Get_Req_Repository.Get_Resource());
+		System.out.println(ResponseBody);
+	}
+}
